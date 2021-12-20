b6c84387a3bd8eec08d73a131cb201fc8e7cddd8
ankur <ankur.dwivedi199714@gmail.com>
2021-12-20 15:23:15 +0530

    resolve error

--- src/modules/Track/router.js
+++ src/modules/Track/router.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const trackController = require("./controller");
 const { withAuthUser } = require("../../middlewares/auth");
 const { createTrackContract } = require("./contract");
-const { validate } = require("../../models/Track");
+const { validate } = require("../../middlewares/schema");
 
 const trackRouter = Router();
 
