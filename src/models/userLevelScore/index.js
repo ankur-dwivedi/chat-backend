9fc275db81ff70cf2d01a8cdb164026bf7d8dc11
ankur <ankur.dwivedi199714@gmail.com>
2022-02-14 17:19:14 +0530

    correct userLevel schema name

--- src/models/userLevelScore/index.js
+++
@@ -1,6 +0,0 @@
-const mongoose = require("mongoose");
-const userLevelOverdueSchema = require("./schema")
-
-const userLevelOverdue = mongoose.model("userLevelOverdue", userLevelOverdueSchema);
-
-module.exports = userLevelOverdue;
