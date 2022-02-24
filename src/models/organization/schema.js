5eede0da05829225806af5702c5d4ce1699fb79c
ankur <ankur.dwivedi199714@gmail.com>
2022-02-24 14:11:27 +0530

    setup revenue scheme in organization

--- src/models/organization/schema.js
+++ src/models/organization/schema.js
@@ -1,10 +1,12 @@
 const { Schema } = require("mongoose");
+const { REVENUE_SCHEME_ENUM } = require("./constants");
 
 const OrganizationSchema = new Schema(
   {
     name: { type: String, required: true },
     logo: { type: String },
     domain: { type: String, required: true, unique: true },
+    revenueScheme: { type: String, trim: true, enum: REVENUE_SCHEME_ENUM },
   },
   { timestamps: true }
 );
