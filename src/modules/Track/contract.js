df4e457b0829586b2fe4ebbd832310aa24ed736e
ankur <ankur.dwivedi199714@gmail.com>
2022-03-11 00:27:08 +0530

    changed group id back to array in track schema

--- src/modules/Track/contract.js
+++ src/modules/Track/contract.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 exports.createTrackContract = Joi.object({
   trackName: Joi.string().required(),
-  groupId:Joi.allow(),
+  groupId: Joi.array().items(Joi.string().required()).required(),
   selectedTheme: Joi.string().required(),
   skillTag: Joi.array(),
   description: Joi.string().required(),
