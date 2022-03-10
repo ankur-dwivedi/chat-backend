b98bed25499bbb85f665555a7b64c3979adc6772
Rahul R Ghimire <ghimirerahul@gmail.com>
2022-03-10 17:25:38 +0530

    added some change

--- src/modules/Track/contract.js
+++ src/modules/Track/contract.js
@@ -2,7 +2,7 @@ const Joi = require("joi");
 
 exports.createTrackContract = Joi.object({
   trackName: Joi.string().required(),
-  groupId: Joi.array().items(Joi.string().required()).required(),
+  groupId:Joi.allow(),
   selectedTheme: Joi.string().required(),
   skillTag: Joi.array(),
   description: Joi.string().required(),
