057d398559c2fa76fd3ee1302df00789492f04b3
ankur <ankur.dwivedi199714@gmail.com>
2022-03-11 13:09:20 +0530

    added docker

--- src/modules/organization/contract.js
+++ src/modules/organization/contract.js
@@ -2,14 +2,15 @@ const Joi = require("joi");
 
 exports.createOrganizationContract = Joi.object({
   name: Joi.string().required(),
-  logo: Joi.string().allow(''),
+  logo: Joi.string().allow(""),
+  domain: Joi.string().required(),
 });
 
 exports.editOrganizationContract = Joi.object({
-  id:Joi.string().required(),
+  id: Joi.string().required(),
   name: Joi.string(),
   logo: Joi.string(),
-})
+});
 
 exports.deleteContract = Joi.object({
   id: Joi.string().required(),
