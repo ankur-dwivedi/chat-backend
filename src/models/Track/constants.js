261f83b7a148f2885cf561a56a642507703cd999
Rahul R Ghimire <ghimirerahul@gmail.com>
2022-03-13 18:45:56 +0530

    added create track with learner ids

---
+++ src/models/Track/constants.js
@@ -0,0 +1,1 @@
+export const booleanEnum = [true,false]
--- src/modules/Track/contract.js
+++ src/modules/Track/contract.js
@@ -2,8 +2,17 @@ const Joi = require("joi");
 
 exports.createTrackContract = Joi.object({
   trackName: Joi.string().required(),
-  groupId: Joi.array().items(Joi.string().required()).required(),
+  groupId: Joi.array().items(Joi.string().required()).allow(),
   selectedTheme: Joi.string().required(),
   skillTag: Joi.array(),
   description: Joi.string().required(),
+});
+
+exports.createTrackUsingLearnerIdContract = Joi.object({
+  trackName: Joi.string().required(),
+  groupId: Joi.array().items(Joi.string().required()).allow(),
+  selectedTheme: Joi.string().required(),
+  skillTag: Joi.array(),
+  description: Joi.string().required(),
+  learnerIds:Joi.array().required(),
 });
