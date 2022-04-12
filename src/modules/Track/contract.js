9fa8c61d0887ad7960dc6f6a981c066360e77ac7
Rahul R Ghimire <ghimirerahul@gmail.com>
2022-04-12 20:14:34 +0530

    'done'

--- src/modules/Track/contract.js
+++ src/modules/Track/contract.js
@@ -16,6 +16,16 @@ exports.createTrackUsingLearnerIdContract = Joi.object({
   learnerIds: Joi.array().required(),
 });
 
+exports.updateTrackUsingLearnerIdContract = Joi.object({
+  trackName: Joi.string().required(),
+  selectedTheme: Joi.string().required(),
+  skillTag: Joi.array().allow(),
+  description: Joi.string().required(),
+  learnerIds: Joi.array().required(),
+  deleteGroupId: Joi.array().allow(),
+  organization: Joi.string().allow()
+});
+
 exports.transferTrackOwnerContract = Joi.object({
   newUserId: Joi.string().required(),
   trackId: Joi.array().required(),
