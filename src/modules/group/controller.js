0d6fd0349645f4a65d63218093fa0baa679c58f3
ankur <ankur.dwivedi199714@gmail.com>
2022-04-27 14:32:30 +0530

    genral test

--- src/modules/group/controller.js
+++ src/modules/group/controller.js
@@ -1,146 +1,243 @@
-const jwt = require("jsonwebtoken");
-const httpErrors = require("httperrors");
-const csv = require("csvtojson");
-const request = require("request");
-const Group = require("../models/group/services");
-const Track = require("../models/Track/services");
-const Level = require("../models/level/services");
-const { getLatestUserLevelByLevel } = require("../models/userLevel/services");
-const User = require("../models/user/services");
-const { LEVEL_STATUS } = require("../models/userLevel/constants");
-const { frequencyData, passFailData } = require("./constants");
-const multer = require("multer");
+const { get, create, deleteGroup, update } = require("../../models/group/services");
+const { removeTrackGroupId } = require("../../models/Track/services");
+const {
+  getGroupEmployee,
+  addGroupId,
+  updateUserByIds,
+  findIdByEmloyeeId,
+  findByGroupId,
+  removeGroupId,
+} = require("../../models/user/services");
+const User = require("../../models/user/services");
+const { createGroupFilterQuery } = require("../../models/user/utils");
+const { csvToJson, csvToJsonByStream } = require("../../utils/general");
+const { uploadFiles } = require(".././../libs/aws/upload");
 
-exports.generateAccessToken = (userId) =>
-  jwt.sign({ userId: userId }, process.env.ACCESS_TOKEN_SECRET, {
-    expiresIn: "7d",
-  });
+exports.getGroups = async (req, res) =>
+  get({ ...req.query, createdBy: req.user._id }).then((group) =>
+    res.send({
+      status: 200,
+      success: true,
+      data: group,
+    })
+  );
 
-exports.generateRefreshToken = (userId) =>
-  jwt.sign({ userId: userId }, process.env.REFRESH_TOKEN_SECRET, {
-    expiresIn: "90d",
-  });
+exports.getGroupById = async (req, res) =>
+  get({ id: req.query.id, createdBy: req.user._id }).then((group) =>
+    res.send({
+      status: 200,
+      success: true,
+      data: group,
+    })
+  );
 
-exports.createUnauthorizedError = (error = "Unauthorized") => httpErrors(401, error);
-
-exports.csvToJson = async (csvUrl) => {
-  let jsonArray = [];
-  await csv()
-    .fromStream(request.get(csvUrl))
-    .subscribe((json) => {
-      return new Promise((resolve, reject) => {
-        jsonArray.push(json);
-        resolve();
+exports.create = async (req, res) => {
+  try {
+    const group = await create({ ...req.body, createdBy: req.user._id }).then((group) => group);
+    let employees = [];
+    if (
+      req.body.properties.length === 1 &&
+      req.body.properties[0].name === "All Employees" &&
+      req.body.properties[0].value[0] === "All Employees"
+    ) {
+      employees = await User.getEmpByOrg({ organization: req.user.organization });
+      const employeeIds = employees.map((value) => value._id);
+      await update({ $and: [{ _id: group._id }] }, { employees: employeeIds });
+      const temp = employeeIds.map((data) => {
+        return { _id: data };
       });
-    });
-  return jsonArray;
-};
-
-exports.csvToJsonByStream = async (csvUrl) => {
-  let jsonArray = [];
-  await csv()
-    .fromFile(csvUrl)
-    .subscribe((json) => {
-      return new Promise((resolve, reject) => {
-        jsonArray.push(json);
-        resolve();
+      await addGroupId({ $or: temp }, group._id);
+      return res.send({
+        status: 200,
+        success: true,
+        data: { group },
       });
-    });
-  return jsonArray;
-};
-
-const getAuthorisedUser = async ({ groupId, trackId }) => {
-  if (groupId) {
-    const group = await Group.get({ id: groupId });
-    return group.employees;
-  } else if (trackId) {
-    const track = await Track.getUsersByTrackId({ id: trackId });
-    let employees = track.groupId.reduce((ar, data) => [...ar, ...data.employees], []);
-    employees = employees.reduce((ar, data) => [...ar, data._id], []);
-    return employees;
+    } else {
+      employees = await getGroupEmployee(req.user.organization, req.body.properties);
+      const employeeIds = employees.map((value) => value._id);
+      await update({ $and: [{ _id: group._id }] }, { employees: employeeIds });
+      await addGroupId(
+        createGroupFilterQuery(req.user.organization, req.body.properties),
+        group._id
+      );
+      return res.send({
+        status: 200,
+        success: true,
+        data: { group },
+      });
+    }
+  } catch (error) {
+    console.log(error.message);
+    if (error.message.indexOf("name_1") !== -1)
+      return res.status(406).json({
+        status: "failed",
+        message: `Group Name needs to be unique`,
+      });
+    return res.status(400).send({ message: `Group already exists` });
   }
 };
 
-const getEmpAttemptData = async ({ groupId, trackId, levelId }) => {
-  const employees = await getAuthorisedUser({ groupId, trackId });
-  let empData = employees.map(async (id) => {
-    const userLevel = await getLatestUserLevelByLevel({
-      levelId,
-      learnerId: id,
+exports.deleteGroup = async (req, res) => {
+  try {
+    // const groupUserId = groupUsers.reduce((ids, data) => [...ids, data._id], []);
+    await removeGroupId({ groupId: req.body.id });
+    //remove group id from track
+    await removeTrackGroupId({ groupId: req.body.id });
+
+    const group = await deleteGroup(req.body.id);
+    return group.deletedCount
+      ? res.send({
+          status: 200,
+          success: true,
+          data: "Group deleted",
+        })
+      : res.status(400).send("Group aleready deleted or doesnt exist");
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: `group deleted` });
+  }
+};
+exports.createGroupEmployee = async (req, res) => {
+  try {
+    const { files } = req;
+    if (!files.length) res.status(400).send("No file uploaded.");
+    const finalbucket =
+      `${process.env.AWS_BUCKET_NAME}` + "/" + `${req.query.org}` + "/employee-data";
+    const uploadedFiles = await uploadFiles(finalbucket, files);
+    const employeeData = await csvToJson(uploadedFiles[0].Location);
+    let employeeIds = await findIdByEmloyeeId(employeeData, req.user.organization);
+    if (!employeeData.length || !employeeData[0].employeeId)
+      return res.status(400).send({ message: `Invalid file format` });
+    const employeeId = employeeData.map((data) => data.employeeId);
+    const employeIdNotFound = await checkEmpIdDontExsist({
+      employeeId,
+      organization: req.user.organization,
     });
-    return userLevel.length
-      ? {
-          ...JSON.parse(JSON.stringify(userLevel[0])),
-          session: userLevel.length,
-        }
-      : null;
-  });
-  empData = await Promise.all(empData);
-  return empData;
+    if (employeIdNotFound.length)
+      return res.status(400).send({ message: `employee not found`, data: employeIdNotFound });
+    const group = await create({
+      ...req.body,
+      employees: employeeIds,
+      createdBy: req.user._id,
+    });
+    await updateUserByIds(req.user.organization, employeeIds, group._id);
+    return res.send({
+      status: 200,
+      success: true,
+      data: group,
+    });
+  } catch (error) {
+    console.log(error);
+    if (error.message.indexOf("name_1") !== -1)
+      return res.status(406).json({
+        status: "failed",
+        message: `Group Name needs to be unique`,
+      });
+    res.status(400).send({ message: `group already exists` });
+  }
 };
 
-exports.analyicsData = async ({ groupId, trackId, levelId }) => {
-  passFailData[0].passed = 0;
-  passFailData[1].failed = 0;
-  passFailData[2].unattempted = 0;
-  for (let x = 0; x <= 9; x++) frequencyData[x].frequency = 0;
-  const empData = await getEmpAttemptData({ groupId, trackId, levelId });
-  empData.map((data) => {
-    if (data !== null && data.levelStatus) {
-      switch (data.levelStatus) {
-        case LEVEL_STATUS.PASS:
-          passFailData[0].passed += 1;
-          break;
-        case LEVEL_STATUS.FAIL:
-          passFailData[1].failed += 1;
-          break;
+exports.countEmpInCsv = async (req, res) => {
+  try {
+    const { files } = req;
+    if (!files.length) return res.status(400).send("No file uploaded.");
+    const employeeData = await csvToJsonByStream(files[0].path);
+    if (!employeeData.length || !employeeData[0].employeeId)
+      return res.status(400).send({ message: `Invalid file format` });
+    const employeeId = employeeData.map((data) => data.employeeId);
+    const employeIdNotFound = await checkEmpIdDontExsist({
+      employeeId,
+      organization: req.user.organization,
+    });
+    if (employeIdNotFound.length)
+      return res.status(400).send({ message: `employee not found`, data: employeIdNotFound });
+    return res.send({
+      status: 200,
+      success: true,
+      data: employeeData.length,
+    });
+  } catch (error) {
+    console.log(error);
+    res.status(400).send({ message: `no employee data found` });
+  }
+};
+
+exports.createGpByEmpList = async (req, res) => {
+  try {
+    const { employeeId } = req.body;
+    const group = await create({
+      ...req.body,
+      employees: employeeId,
+      createdBy: req.user._id,
+    });
+    await updateUserByIds(req.user.organization, employeeId, group._id);
+    return res.send({
+      status: 200,
+      success: true,
+      data: group,
+    });
+  } catch (error) {
+    console.log(error);
+    if (error.message.indexOf("name_1") !== -1)
+      return res.status(406).json({
+        status: "failed",
+        message: `Group Name needs to be unique`,
+      });
+    res.status(400).send({ message: `group already exists` });
+  }
+};
+
+const checkEmpIdDontExsist = async ({ employeeId, organization }) => {
+  const employeIdNotFound = [];
+  let emp = employeeId.map(async (id) => {
+    const empData = await User.getEmpBempIdOrg({
+      employeeId: id,
+      organization,
+    });
+    if (!empData) employeIdNotFound.push(id);
+    return id;
+  });
+  emp = await Promise.all(emp);
+  return employeIdNotFound;
+};
+
+exports.update = async (req, res) => {
+  try {
+    const group = await get({ id: req.body.id, createdBy: req.user._id });
+    const old = group.employees.map((data) => data._id.toString());
+    console.log(group);
+    if (!group) return res.status(400).send({ message: `Invalid group id` });
+    if (req.body.employees && req.body.employees.length) {
+      if (old && old.length) {
+        let empIdTobeRemoved = old.filter(
+          (data) => !new Set(req.body.employees).has(data.toString())
+        );
+        if (empIdTobeRemoved.length)
+          await User.removeGroupIdByEmpId({
+            groupId: req.body.id,
+            employeeId: empIdTobeRemoved,
+          });
       }
-    } else passFailData[2].unattempted += 1;
-
-    if (data !== null && data.levelScore) {
-      const index = Math.abs(data.levelScore / 10) !== 0 ? Math.abs(data.levelScore / 10) - 1 : 0;
-      frequencyData[index].frequency += 1;
+      const empIdTobeAdded = req.body.employees.filter((data) => !new Set(old).has(data));
+      if (empIdTobeAdded.length)
+        await updateUserByIds(req.user.organization, empIdTobeAdded, req.body.id);
     }
-  });
-  return { passFailData, frequencyData };
+    const queryObject = { $and: [{ _id: req.body.id }] };
+    const updateObject = { ...req.body };
+    delete updateObject.id;
+    const updateGroup = await update(queryObject, updateObject).then((user) => ({
+      status: 200,
+      success: true,
+      data: "Group updated successfully",
+    }));
+    return res.send(updateGroup);
+  } catch (error) {
+    console.log(error);
+    if (error.message.indexOf("name_1") !== -1)
+      return res.status(406).json({
+        status: "failed",
+        message: `Group Name needs to be unique`,
+      });
+    res.status(400).send({ message: error.message });
+  }
 };
-
-exports.analyicslist = async ({ groupId, trackId, levelId }) => {
-  let empData = await getEmpAttemptData({ groupId, trackId, levelId });
-  const level = await Level.get({ id: levelId });
-  const track = await Track.get({ id: trackId });
-  empData = empData.filter((data) => {
-    if (data) return data;
-  });
-  empData = empData.map(async (data) => {
-    if (data !== null) {
-      const userData = await User.getUserAnalytics({ id: data.learnerId });
-      return {
-        group: userData.groups,
-        track: track.trackName,
-        level: level.levelName,
-        name: userData.name,
-        employeeId: userData.employeeId,
-        score: data.levelScore,
-        status: data.levelStatus,
-        numOfAttempts: data.session,
-        dateOfAttempt: data.updatedAt,
-      };
-      return { user: userData, userLevel: empData };
-    }
-  });
-  return await Promise.all(empData);
-};
-
-exports.generateOtp = () => Math.floor(100000 + Math.random() * 900000);
-
-exports.upload = multer({
-  storage: multer.diskStorage({
-    destination: function (req, file, cb) {
-      cb(null, "/tmp");
-    },
-    filename: function (req, file, cb) {
-      cb(null, file.originalname);
-    },
-  }),
-});
