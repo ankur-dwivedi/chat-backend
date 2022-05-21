b9b7c9d34084152b729c76dff12aac2ff221e5af
saswatmishra811 <saswatmishra1996@gmail.com>
2022-05-22 01:17:21 +0530

    fixed typo

--- src/modules/user/util.js
+++ src/modules/user/util.js
@@ -124,7 +124,7 @@ const processPaginatedResults = (data) => {
     totalCount: data[0].totalCount[0].totalCount,
     data: data[0].data.map((user) => {
       return {
-        employeeId: user.emplpyeeId,
+        employeeId: user.employeeId,
         name: user.name,
         role: user.role,
         email: user.email,
