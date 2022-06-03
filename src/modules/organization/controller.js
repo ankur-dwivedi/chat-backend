4239196fee9ada95b3cb4e3a03f321f641ef162e
saswatmishra811 <saswatmishra1996@gmail.com>
2022-06-04 02:31:01 +0530

    added count-replace-org-data api

--- src/modules/organization/controller.js
+++ src/modules/organization/controller.js
@@ -9,11 +9,11 @@ const {
   create: userCreate,
   countEmployeeInOrg,
   findIdByEmloyeeId,
-   createUserAfterReplace
+  createUserAfterReplace,
 } = require("../../models/user/services");
 const { uploadFiles } = require(".././../libs/aws/upload");
 const { csvToJson, csvToJsonByStream } = require("../../utils/general");
-const { createUserObject } = require("./utils");
+const { createUserObject, removeDuplicates } = require("./utils");
 const User = require("../../models/user");
 const { get: getFilterData } = require("../../models/filterData/services");
 
@@ -39,17 +39,24 @@ exports.create = async (req, res) =>
       return res.status(400).send({ message: `organization already exists` });
     });
 
-exports.deleteOrganization = async (req, res) => deleteOrganization(req.body.id).then((organization) => (organization.deletedCount ? res.send("Organization deleted") : res.send("Organization aleready deleted or doesnt exist")));
+exports.deleteOrganization = async (req, res) =>
+  deleteOrganization(req.body.id).then((organization) =>
+    organization.deletedCount
+      ? res.send("Organization deleted")
+      : res.send("Organization aleready deleted or doesnt exist")
+  );
 
 exports.update = async (req, res) => {
   const queryObject = { $and: [{ _id: req.body.id }] };
   const updateObject = { ...req.body, updatedAt: new Date() };
   delete updateObject.id;
-  const updateOrganization = await update(queryObject, updateObject).then((organization) => ({
-    status: 200,
-    success: true,
-    data: organization,
-  }));
+  const updateOrganization = await update(queryObject, updateObject).then(
+    (organization) => ({
+      status: 200,
+      success: true,
+      data: organization,
+    })
+  );
   return res.send(updateOrganization);
 };
 
@@ -58,7 +65,8 @@ exports.uploadLogo = async function (req, res) {
     const { files } = req;
     if (!files.length) res.status(400).send("No file uploaded.");
 
-    let finalbucket = `${process.env.AWS_BUCKET_NAME}` + "/" + `${req.query.org}` + "/logo";
+    let finalbucket =
+      `${process.env.AWS_BUCKET_NAME}` + "/" + `${req.query.org}` + "/logo";
     let uploadedFiles = await uploadFiles(finalbucket, files);
     const queryObject = { $and: [{ _id: req.query.org }] };
     const updateObject = {
@@ -66,7 +74,9 @@ exports.uploadLogo = async function (req, res) {
       updatedAt: new Date(),
     };
     delete updateObject.id;
-    await update(queryObject, updateObject).then((organization) => organization);
+    await update(queryObject, updateObject).then(
+      (organization) => organization
+    );
     res.status(200).send({
       status: "success",
       message: "files uploaded successfully",
@@ -81,10 +91,16 @@ exports.uploadEmployeeData = async function (req, res) {
   try {
     const { files } = req;
     if (!files.length) return res.status(400).send("No file uploaded.");
-    const finalbucket = `${process.env.AWS_BUCKET_NAME}` + "/" + `${req.query.org}` + "/employee-data";
+    const finalbucket =
+      `${process.env.AWS_BUCKET_NAME}` +
+      "/" +
+      `${req.query.org}` +
+      "/employee-data";
     const uploadedFiles = await uploadFiles(finalbucket, files);
     const employeeData = await csvToJson(uploadedFiles[0].Location);
-    const updatedData = employeeData.map((value) => createUserObject(req.query.org, value, req.query.role));
+    const updatedData = employeeData.map((value) =>
+      createUserObject(req.query.org, value, req.query.role)
+    );
     const userNotCreated = [],
       userCreated = [];
     for (value of updatedData) {
@@ -188,7 +204,7 @@ exports.addUsersBulk = async (req, res) => {
           const num = value.phoneNumber;
           if (value.email === "") delete value.email;
           const cre = await userCreate({ ...value, phoneNumber: Number(num) });
-           userCreated.push(cre);
+          userCreated.push(cre);
         }
       } catch (err) {
         console.log(value, err.message, userNotCreated.length);
@@ -202,7 +218,110 @@ exports.addUsersBulk = async (req, res) => {
     });
   } catch (error) {
     res.status(400).send({ message: error.message });
+  }
+};
+
+exports.countReplaceEmployeeData = async (req, res) => {
+  try {
+    const { files } = req;
+    const org = req.query.org ? req.query.org : req.user.organization;
+    console.log(org);
+    if (!files.length) return res.status(400).send("No file uploaded.");
+    const employeeData = await csvToJsonByStream(files[0].path);
+    const processedEmployeeData = await removeDuplicates(employeeData);
+    const userNotCreated = [],
+      userCreated = [],
+      existingUser = [],
+      newUser = [];
+    const updatedEmpData = [];
+
+    for (emp of processedEmployeeData) {
+      let user = {},
+        updateUser;
+
+      //  check existing data to retain
+      if (emp.email && emp.phoneNumber)
+        user = await User.findOne({
+          $or: [
+            { $and: [{ email: emp.email }, { organization: org }] },
+            {
+              $and: [{ phoneNumber: emp.phoneNumber }, { organization: org }],
+            },
+          ],
+        });
+      else if (emp.email)
+        user = await User.findOne({
+          $and: [{ email: emp.email }, { organization: org }],
+        });
+      else if (emp.phoneNumber)
+        user = await User.findOne({
+          $and: [{ phoneNumber: emp.phoneNumber }, { organization: org }],
+        });
+      if (user) {
+        existingUser.push(user);
+        if (user.employeeId === emp.employeeId && user.organization === org) {
+          updateUser = {
+            _id: user._id,
+            organization: user?.organization,
+            groups: user?.groups,
+            currentState: user?.currentState,
+            password: user?.password,
+          };
+        } else {
+          updateUser = {
+            _id: user._id,
+            organization: user?.organization,
+            groups: user?.groups,
+            currentState: user?.currentState,
+          };
+        }
+      } else {
+        newUser.push(createUserObject(org, emp, req.query.role));
+      }
+
+      // create user object to create
+      updatedEmpData.push(
+        updateUser
+          ? {
+              ...createUserObject(org, emp, req.query.role),
+              ...updateUser,
+            }
+          : createUserObject(org, emp, req.query.role)
+      );
     }
+
+    for (value of updatedEmpData) {
+      try {
+        if (!value.phoneNumber && !value.email) userNotCreated.push(value);
+        if (Number(value.phoneNumber) === 0) {
+          delete value.phoneNumber;
+          if (value.email === "") delete value.email;
+          userCreated.push(value);
+        } else {
+          const num = value.phoneNumber;
+          if (value.email === "") delete value.email;
+          // create new users for org
+          userCreated.push(value);
+        }
+      } catch (err) {
+        userNotCreated.push(value);
+      }
+    }
+    return res.status(200).send({
+      status: "success",
+      message: "files uploaded successfully",
+      data: {
+        newTotalUserCount: userCreated.length,
+        newUserCount: newUser.length,
+        existingUserCount: existingUser.length,
+        invalidUserCount:
+          userNotCreated.length +
+          (employeeData.length - processedEmployeeData.length),
+      },
+    });
+  } catch (error) {
+    return res.status(406).send({ message: error.message });
+  }
 };
 
 // replace employeed Data with csv retain if email or phone found
@@ -211,7 +330,7 @@ exports.replaceEmployeeData = async function (req, res) {
     const { files } = req;
     if (!files.length) return res.status(400).send("No file uploaded.");
     const employeeData = await csvToJsonByStream(files[0].path);
-
+    const org = req.query.org ? req.query.org : req.user.organization;
     // upload csv to s3 bucket
     // const finalbucket = `${process.env.AWS_BUCKET_NAME}` + "/" + `${req.query.org}` + "/employee-data";
     // await uploadFiles(finalbucket, files);
@@ -224,25 +343,57 @@ exports.replaceEmployeeData = async function (req, res) {
         updateUser;
 
       //  check existing data to retain
-      if (emp.email && emp.phoneNumber) user = await User.findOne({ $or: [{ $and: [{ email: emp.email }, { organization: req.query.org }] }, { $and: [{ phoneNumber: emp.phoneNumber }, { organization: req.query.org }] }] });
-      else if (emp.email) user = await User.findOne({ $and: [{ email: emp.email }, { organization: req.query.org }] });
-      else if (emp.phoneNumber) user = await User.findOne({ $and: [{ phoneNumber: emp.phoneNumber }, { organization: req.query.org }] });
+      if (emp.email && emp.phoneNumber)
+        user = await User.findOne({
+          $or: [
+            { $and: [{ email: emp.email }, { organization: org }] },
+            {
+              $and: [{ phoneNumber: emp.phoneNumber }, { organization: org }],
+            },
+          ],
+        });
+      else if (emp.email)
+        user = await User.findOne({
+          $and: [{ email: emp.email }, { organization: org }],
+        });
+      else if (emp.phoneNumber)
+        user = await User.findOne({
+          $and: [{ phoneNumber: emp.phoneNumber }, { organization: org }],
+        });
       if (user) {
-        if (user.employeeId === emp.employeeId && user.organization == req.query.org) {
-          console.log('enter 1',user.employeeId,' ',emp.employeeId)
-          updateUser = { _id: user._id, organization: user?.organization, groups: user?.groups, currentState: user?.currentState, password: user?.password };
+        if (user.employeeId === emp.employeeId && user.organization === org) {
+          updateUser = {
+            _id: user._id,
+            organization: user?.organization,
+            groups: user?.groups,
+            currentState: user?.currentState,
+            password: user?.password,
+          };
         } else {
-          console.log('enter 2')
-          updateUser = { _id: user._id, organization: user?.organization, groups: user?.groups, currentState: user?.currentState };
+          updateUser = {
+            _id: user._id,
+            organization: user?.organization,
+            groups: user?.groups,
+            currentState: user?.currentState,
+          };
         }
       }
 
       // create user object to create
-      updatedEmpData.push(updateUser ? { ...createUserObject(req.query.org, emp, req.query.role), ...updateUser } : createUserObject(req.query.org, emp, req.query.role));
+      updatedEmpData.push(
+        updateUser
+          ? {
+              ...createUserObject(org, emp, req.query.role),
+              ...updateUser,
+            }
+          : createUserObject(org, emp, req.query.role)
+      );
     }
 
     // delete existing user data for org
-    await User.deleteMany({ organization: req.query.org }).then((response) => console.log({ response }));
+    await User.deleteMany({ organization: org }).then((response) =>
+      console.log({ response })
+    );
 
     for (value of updatedEmpData) {
       try {
@@ -257,7 +408,10 @@ exports.replaceEmployeeData = async function (req, res) {
           const num = value.phoneNumber;
           if (value.email === "") delete value.email;
           // create new users for org
-          const cre = await createUserAfterReplace({ ...value, phoneNumber: Number(num) });
+          const cre = await createUserAfterReplace({
+            ...value,
+            phoneNumber: Number(num),
+          });
           userCreated.push(cre);
         }
       } catch (err) {
@@ -274,4 +428,4 @@ exports.replaceEmployeeData = async function (req, res) {
     // console.log(error);
     return res.status(400).send({ message: error.message });
   }
-};    
+};
